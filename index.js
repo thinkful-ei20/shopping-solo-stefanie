0c8cdfa4d2aff06213b009ecb09c415fba597524
Stefanie Carrasco <stefanieanncarrasco@gmail.com>
2018-04-06 16:11:35 -0400

    first push

---
+++ index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+/* global $ */
+
+// `STORE` is responsible for storing the underlying data
+// that our app needs to keep track of in order to work.
+//
+// for a shopping list, our data model is pretty simple.
+// we just have an array of shopping list items. each one
+// is an object with a `name` and a `checked` property that
+// indicates if it's checked off or not.
+// we're pre-adding items to the shopping list so there's
+// something to see when the page first loads.
+const STORE = [
+  {name: 'apples', checked: false},
+  {name: 'oranges', checked: false},
+  {name: 'milk', checked: true},
+  {name: 'bread', checked: false}
+];
+
+function generateItemElement(item, itemIndex, template){
+  return `
+  <li class="js-item-index-element" data-item-index="${itemIndex}">
+    <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle js-item-toggle">
+          <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete js-item-delete">
+          <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`;
+}
+  
+function generateShoppingItemsString(shoppingList) {
+  console.log('Generating shopping list element');
+  const items = shoppingList.map((item, index) => generateItemElement(item, index));
+  return items.join('');
+}
+
+function renderShoppingList(){
+  //this will render the shopping list in the DOM
+  console.log('renderShoppingList ran');
+  //defining the string as function above with STORE as shopping list parameter
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+  //insert the HTML into the DOM below
+  $('.js-shopping-list').html(shoppingListItemsString);
+}
+
+function handleNewItemSubmit() {
+  //this will be responsible for when users add a new
+  //item to the shopping list
+  console.log('handleNewSubmitItem ran');
+}
+
+function handleItemCheckClicked() {
+  //this function will be responsible for when users
+  //click the "check" button on a shopping list item
+  console.log('handleItemCheckClicked ran');
+}
+
+function handleDeleteItemClicked() {
+  //responsible for when users want to delete an item
+  //from the list
+  console.log('handleDeleteItemClicked ran');
+}
+
+// this function will be our callback when the page loads. it's responsible for
+// initially rendering the shopping list, and activating our individual functions
+// that handle new item submission and user clicks on the "check" and "delete" buttons
+// for individual shopping list items.
+function handleShoppingList() {
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
+}
+
+// when the page loads, call `handleShoppingList`
+$(handleShoppingList);
